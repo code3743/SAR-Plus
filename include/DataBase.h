deb5e6bf95fd89e24494b0d37e9fcbafd4d1a74d
code3743 <jota.lopez@correounivalle.edu.co>
2022-09-21 08:26:05 -0500

    Inicio proyecto

---
+++ include/DataBase.h
@@ -0,0 +1,16 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+
+class DataBase
+{
+    public:
+        DataBase();
+        virtual ~DataBase();
+
+    protected:
+
+    private: 
+};
+
+#endif // DATABASE_H
